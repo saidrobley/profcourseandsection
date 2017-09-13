aa8721fc70239455c64af059beb0d13cf61b3cef
Said Robley <saidrobley@gmail.com>
2017-09-12 19:04:58 -0600

    created one to many relationship between professor and sections, course and sections, modified sections/views _form.html.erb, index.htm.erb, and show.html.erb

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,4 +1,9 @@
 class Section < ApplicationRecord
+
+  validates :number, presence: true
+  validates :semester, presence: true
+  validates :room, presence: true
   belongs_to :professor
   belongs_to :course
+  validates_uniqueness_of :number, scope: :course_id
 end
